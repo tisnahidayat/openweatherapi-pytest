97705a745e8304abb7c2f51fb171907a50368876
tisnahidayat__ <tisnahidayat150302@gmail.com>
2025-07-23 15:35:00 +0700

    Setup project pytest

---
+++ utils/api_client.py
@@ -0,0 +1,23 @@
+import requests as req
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+class APIClient:
+    def __init__(self):
+        self.base_url = os.getenv("BASE_URL")
+        self.api_key = os.getenv("API_KEY")
+        if not self.base_url:
+            raise ValueError("BASE_URL must be set in the .env file")
+        self.headers = {
+            "Content-Type": "application/json"
+        }
+    def get(self, endpoint, params=None):
+        url = f"{self.base_url}/{endpoint}"
+        params = {"appid": self.api_key, **(params or {})}
+        response = req.get(url, headers=self.headers, params=params)
+        response.raise_for_status()
+        return response.json()
+    
